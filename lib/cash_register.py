fa0ac6a2bc4a62b7e0fa759334b85611fa5bc949
jimmyjaxon <jimmyjaxon695@gmail.com>
2026-06-10 10:03:21 +0300

    Implement CashRegister class with all methods

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self._last_transaction = 0
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([title] * quantity)
+        self._last_transaction = price * quantity
+
+    def apply_discount(self):
+        if self.discount == 0:
+            print("There is no discount to apply.")
+        else:
+            self.total = self.total - (self.total * self.discount / 100)
+            print(f"After the discount, the total comes to ${int(self.total)}.")
+
+    def void_last_transaction(self):
+        self.total -= self._last_transaction
+        self._last_transaction = 0
